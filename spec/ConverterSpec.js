fa20f2339d9ab1b82d2a5d6c269d8aff62129e28
jkbits1 <jkbits1@users.noreply.github.com>
2014-07-10 03:23:43 +0100

    second test for F to C conversion

--- spec/ConverterSpec.js
+++ spec/ConverterSpec.js
@@ -6,5 +6,12 @@ describe("Cooking Ingredients Converter", function() {
 
       expect(result).toEqual('37.8');
     });
+
+    it("F85 to C", function() {
+
+        var result = convertFahrenheitToCelsius(85);
+
+        expect(result).toEqual('29.4');
+    });
   });
 });
